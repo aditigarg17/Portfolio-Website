2ec426a2abed397148cef8fdc2acb214c0ea9e40
aditigarg17 <154456795+aditigarg17@users.noreply.github.com>
2024-04-08 00:20:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-document.getElementById("read-more-btn").addEventListener("click", function() {
-    var moreText = document.querySelector('.more-text');
-    if (moreText.style.display === 'none') {
-        moreText.style.display = 'inline'; // Show the hidden content
-        this.textContent = 'Read Less';
-    } else {
-        moreText.style.display = 'none'; // Hide the content
-        this.textContent = 'Read More';
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button
+    var btnScrollToTop = document.getElementById("btnScrollToTop");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btnScrollToTop.style.display = "block";
+        } else {
+            btnScrollToTop.style.display = "none";
+        }
     }
+
+    // When the user clicks on the button, scroll to the top of the document
+    btnScrollToTop.addEventListener("click", function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
 });
